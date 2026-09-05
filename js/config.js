abb56eaa5aac85c600bf86951e5f7a52027c6920
expertkevin06-cell <expertkevin06@gmail.com>
2026-09-05 21:26:18 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,15 @@
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAkebsuk...",
+  authDomain: "firediag-auto.firebaseapp.com",
+  projectId: "firediag-auto",
+  storageBucket: "firediag-auto.appspot.com",
+  messagingSenderId: "939193182729",
+  appId: "1:939193182729:web:..."
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const GEMINI_API_KEY = "VOTRE_CLE_GEMINI";
