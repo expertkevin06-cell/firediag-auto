c6680eb2daea68ff17e2c8781ada43e2fadeca07
expertkevin06-cell <expertkevin06@gmail.com>
2026-09-05 23:55:28 +0200

    Register service worker with dynamic base path

--- js/service-worker-register.js
+++ js/service-worker-register.js
@@ -1,6 +1,8 @@
 if ('serviceWorker' in navigator) {
+    const basePath = window.location.pathname.includes('/firediag-auto') ? '/firediag-auto' : '';
+    
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
+        navigator.serviceWorker.register(basePath + '/sw.js')
             .then(registration => {
                 console.log('ServiceWorker enregistré:', registration.scope);
             })
